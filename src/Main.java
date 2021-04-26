c1807a97b1143777a4ea690ca7b3b46c46fd7924
syx <424193726@qq.com>
2021-04-26 15:55:23 +0800

    对文件名进行批量处理

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+/**
+ * @author:syx
+ * @date: 2021/4/26 10:29
+ * @version:v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        File f = new File("C:\\Users\\sun\\Desktop\\新建文件夹");
+        File[] files = f.listFiles();
+        for (File f1:files){
+            String oldName = f1.getName();
+            System.out.println(f1.getParentFile()+"\\"+changeName(oldName));
+            f1.renameTo(new File(f1.getParentFile()+"\\"+changeName(oldName)));
+        }
+
+
+    }
+    private static String changeName(String str){
+        StringBuffer sb = new StringBuffer();
+        //sb.append(str.substring(0,6));
+        sb.append(str.substring(13,str.length()-7));
+        sb.append(str.substring(str.length()-4,str.length()));
+        return sb.toString();
+    }
+}
